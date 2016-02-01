8293e6093013a26bb3871115cbc1bdb8ebaad4c8
Sam Potter <sfp@hush.ai>
2016-02-01 16:42:34 -0500

    formatting

--- src/cpp/cauchy.cpp
+++ src/cpp/cauchy.cpp
@@ -2,21 +2,25 @@
 #include <cmath>
 
 nufft::range_elt_type
-nufft::cauchy::R(integer_type m, domain_elt_type x) {
+nufft::cauchy::R(integer_type m, domain_elt_type x)
+{
 	return std::pow(x, m);
 }
 
 nufft::range_elt_type
-nufft::cauchy::S(integer_type m, domain_elt_type x) {
+nufft::cauchy::S(integer_type m, domain_elt_type x)
+{
 	return std::pow(x, -(m + 1));
 }
 
 nufft::range_elt_type
-nufft::cauchy::a(integer_type m, domain_elt_type x) {
+nufft::cauchy::a(integer_type m, domain_elt_type x)
+{
 	return std::pow(-x, -(m + 1));
 }
 
 nufft::range_elt_type
-nufft::cauchy::b(integer_type m, domain_elt_type x) {
+nufft::cauchy::b(integer_type m, domain_elt_type x)
+{
 	return std::pow(x, m);
 }

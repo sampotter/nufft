a31c9fc588ea656b18be2d55e2c0c1e4a80f90ce
Sam Potter <sfp@hush.ai>
2016-04-07 13:19:19 -0400

    more plotting

--- src/py/plot.py
+++ src/py/plot.py
@@ -1,3 +1,4 @@
+import inspect as _inspect
 import groundtruth as _groundtruth
 import matplotlib.pyplot as _plt
 import nufft as _nufft
@@ -16,10 +17,156 @@ def _get_ground_truth_interp(X, F, Y, K):
     KK = _groundtruth.K(X, Y, 1 - K, K - 1)
     return _np.matrix(KK) * _np.matrix(F).transpose()
 
-def rad_vs_per_for_mse_wrt_n(path=None):
+# TODO: plot of error vs location of evaluation point... This would be
+# a useful graph.
+
+# TODO: try different check point distributions
+
+# TODO: different test series -- with respect to what, though...? do this last
+
+def radial_error_vs_target_location(save=False, preamble=False):
+    if preamble:
+        print('#' * 80)
+        print('# ' + _inspect.stack()[0][3])
+        print('#')
+
+    K = 2
+    X = _get_X(K).real
+    J = 500
+    L = 10
+    p = 50
+    q = 10
+    Y = _np.linspace(0, _twopi, J, endpoint=False)
+    F = _testseries.semicircle(X, K).real
+    G_gt = _get_ground_truth_interp(X, F, Y, K).real
+    n_min_power = 1
+    n_max_power = 4
+    num_Ns = 500
+    Ns = _np.logspace(n_min_power, n_max_power, num_Ns)
+
+    digits = _np.matrix(_np.zeros((J, num_Ns), dtype=_np.float64))
+    for i, n in enumerate(Ns):
+        print('n = %d' % n)
+        G_rad = _np.matrix(_groundtruth.inufft_radial_approximation(F, K, Y, L, p, int(n))).T
+        digits[:, i] = -_np.log10(_np.abs(G_gt - G_rad))
+
+    fig = _plt.figure()
+    imshow_ax = _plt.imshow(digits, interpolation='nearest', cmap=_cm.cubehelix)
+    _plt.xticks(_np.linspace(1, num_Ns, n_max_power - n_min_power + 1))
+    _plt.yticks(_np.linspace(1, J, 5))
+    ax = _plt.gca()
+    xticklabels = ['1e%d' % power for power in range(n_min_power, n_max_power + 1)]
+    ax.set_xticklabels(xticklabels)
+    yticklabels = ['0', u'π/2', u'π', u'3π/2', u'2π']
+    ax.set_yticklabels(yticklabels)
+    fig.colorbar(imshow_ax)
+    _plt.title('Number of Correct Digits by Radial Approx. INUFFT (K = %d)' % K)
+    _plt.xlabel('Neighborhood Radius (n)')
+    _plt.ylabel('Evaluation Point (y)')
+    if save:
+        _plt.savefig(_inspect.stack()[0][3] + '.pdf')
+    else:
+        _plt.show()
+
+def per_error_vs_target_location(save=False, preamble=False):
+    if preamble:
+        print('#' * 80)
+        print('# ' + _inspect.stack()[0][3])
+        print('#')
+
+    K = 2
+    X = _get_X(K).real
+    J = 500
+    L = 10
+    p = 50
+    q = 10
+    Y = _np.linspace(0, _twopi, J, endpoint=False)
+    F = _testseries.semicircle(X, K).real
+    G_gt = _get_ground_truth_interp(X, F, Y, K).real
+    n_min_power = 1
+    n_max_power = 4
+    num_Ns = 500
+    Ns = _np.logspace(n_min_power, n_max_power, num_Ns)
+
+    digits = _np.matrix(_np.zeros((J, num_Ns), dtype=_np.float64))
+    for i, n in enumerate(Ns):
+        print('n = %d' % n)
+        G_per = _np.matrix(_nufft.inufft(F, K, Y, L, p, int(n), q)).T
+        digits[:, i] = -_np.log10(_np.abs(G_gt - G_per))
+
+    fig = _plt.figure()
+    imshow_ax = _plt.imshow(digits, interpolation='nearest', cmap=_cm.cubehelix)
+    _plt.xticks(_np.linspace(1, num_Ns, n_max_power - n_min_power + 1))
+    _plt.yticks(_np.linspace(1, J, 5))
+    ax = _plt.gca()
+    xticklabels = ['1e%d' % power for power in range(n_min_power, n_max_power + 1)]
+    ax.set_xticklabels(xticklabels)
+    yticklabels = ['0', u'π/2', u'π', u'3π/2', u'2π']
+    ax.set_yticklabels(yticklabels)
+    fig.colorbar(imshow_ax)
+    _plt.title('Number of Correct Digits by Per. Sum. INUFFT (K = %d)' % K)
+    _plt.xlabel('Neighborhood Radius (n)')
+    _plt.ylabel('Evaluation Point (y)')
+    if save:
+        _plt.savefig(_inspect.stack()[0][3] + '.pdf')
+    else:
+        _plt.show()
+
+def rad_vs_per_wrt_n_with_fixed_Y(save=False, preamble=False):
+    if preamble:
+        print('#' * 80)
+        print('# ' + _inspect.stack()[0][3])
+        print('#')
+
+    K = 20
+    X = _get_X(K)
+    J = 100
+    L = 4
+    p = 4
+    q = 10
+    Y = _np.sort(_np.random.uniform(0, _twopi, J))
+    F = _testseries.semicircle(X, K).real
+    G_gt = _get_ground_truth_interp(X, F, Y, K)
+        
+    min_n = 1
+    max_n = 20
+    Ns = range(min_n, max_n + 1)
+    
+    mses = {
+        'per': _np.zeros((len(Ns), 1), dtype=_np.float64),
+        'rad': _np.zeros((len(Ns), 1), dtype=_np.float64)
+    }
+    for i, n in enumerate(Ns):
+        print('n = %d' % n)
+        G_per = _nufft.inufft(F, K, Y, L, p, n, q)
+        G_rad = _groundtruth.inufft_radial_approximation(F, K, Y, L, p, n)
+        mses['per'][i] = _np.log10(_np.linalg.norm(G_per - G_gt, 2)**2/len(G_gt))
+        mses['rad'][i] = _np.log10(_np.linalg.norm(G_rad - G_gt, 2)**2/len(G_gt))
+
+    _plt.figure()
+    _plt.plot(Ns, mses['per'], label='INUFFT')
+    _plt.plot(Ns, mses['rad'], label='Radial Approx.')
+    _plt.xlim((min_n - 1, max_n + 1))
+    _plt.title('Error vs. Neighborhood Size with Fixed Targets')
+    _plt.xlabel('Neighborhood Size')
+    _plt.ylabel('Log10 MSE')
+    _plt.legend()
+    if save:
+        _plt.savefig(_inspect.stack()[0][3] + '.pdf')
+    else:
+        _plt.show()
+    
+    return Ns, mses
+    
+def rad_vs_per_for_mse_wrt_n(save=False, preamble=False):
     # TODO: this plot indicates that the error due to the position of
     # the evaluation points dwarfs the error due to ignoring the use
     # of least squares collocation.
+
+    if preamble:
+        print('#' * 80)
+        print('# ' + _inspect.stack()[0][3])
+        print('#')
 
     K = 20
     X = _get_X(K)
@@ -53,20 +200,23 @@ def rad_vs_per_for_mse_wrt_n(path=None):
     _plt.figure()
     _plt.plot(Ns, mses['per'], label='INUFFT')
     _plt.plot(Ns, mses['rad'], label='Radial Approx.')
+    _plt.xlim((min_n - 1, max_n + 1))
+    _plt.xlabel('Neighborhood Size')
+    _plt.ylabel('Median of Log10 of MSE (50 trials)')
     _plt.legend()
-    if path:
-        _plt.savefig(path)
+    if save:
+        _plt.savefig(_inspect.stack()[0][3] + '.pdf')
     else:
         _plt.show()
     
     return Ns, mses
 
-# TODO: plot of error vs location of evaluation point... This would be
-# a useful graph.
+def p_vs_n_wrt_mse(save=False, preamble=False):
+    if preamble:
+        print('#' * 80)
+        print('# ' + _inspect.stack()[0][3])
+        print('#')
 
-# TODO: try different check point distributions
-
-def p_vs_n_wrt_mse():
     K = 10
     X = _get_X(K)
     J = 100
@@ -105,13 +255,19 @@ def p_vs_n_wrt_mse():
     _plt.ylabel('Truncation Number')
     _plt.xticks(range(len(Ns)))
     _plt.yticks(range(len(Ps)))
-    _plt.show()
+    if save:
+        _plt.savefig(_inspect.stack()[0][3] + '.pdf')
+    else:
+        _plt.show()
     
     return Ps, Ns, mses
 
-# TODO: FMM depth vs speed, optimal depth
+def L_vs_p_wrt_time(save=False, preamble=False):
+    if preamble:
+        print('#' * 80)
+        print('# ' + _inspect.stack()[0][3])
+        print('#')
 
-def L_vs_p_wrt_time():
     K = 10
     X = _get_X(K)
     J = 100
@@ -146,11 +302,19 @@ def L_vs_p_wrt_time():
     _plt.ylabel('FMM Depth')
     _plt.xticks(range(len(Ps)))
     _plt.yticks(range(len(Ls)))
-    _plt.show()
+    if save:
+        _plt.savefig(_inspect.stack()[0][3] + '.pdf')
+    else:
+        _plt.show()
     
     return Ls, Ps, times
 
-def rad_vs_persum_with_n_wrt_mse():
+def rad_vs_persum_with_n_wrt_mse(save=False, preamble=False):
+    if preamble:
+        print('#' * 80)
+        print('# ' + _inspect.stack()[0][3])
+        print('#')
+
     K = 10
     X = _get_X(K)
     J = 100
@@ -182,16 +346,27 @@ def rad_vs_persum_with_n_wrt_mse():
         mses[i, 1] = _np.median(_mses[:, 1])
 
     _plt.figure()
-    _plt.plot(range(len(Ns)), mses[:, 0]/mses[:, 1], 'o')
-    _plt.xticks(range(len(Ns)))
-    _plt.xlim((0, 19))
-    _plt.show()
+    plotrange = range(len(Ns))
+    _plt.plot(plotrange, mses[:, 0]/mses[:, 1], 'o')
+    _plt.xticks(plotrange)
+    _plt.xlim((plotrange[0] - 1, plotrange[-1] + 1))
+    _plt.ylabel('Log10 MSE of Per. Sum. over Log10 MSE of Radial')
+    _plt.xlabel('Neighborhood Size')
+    ax = _plt.gca()
+    ax.set_xticklabels([''] + [str(n) for n in Ns] + [''])
+    if save:
+        _plt.savefig(_inspect.stack()[0][3] + '.pdf')
+    else:
+        _plt.show()
 
     return Ns, mses
 
-# TODO: ground truth vs radial approx vs persum wrt times
+def gt_vs_rad_vs_per_with_bandlimit_wrt_time(save=False, preamble=False):
+    if preamble:
+        print('#' * 80)
+        print('# ' + _inspect.stack()[0][3])
+        print('#')
 
-def gt_vs_rad_vs_per_with_bandlimit_wrt_time():
     q = 100
     L = 4
     p = 4
@@ -218,15 +393,19 @@ def gt_vs_rad_vs_per_with_bandlimit_wrt_time():
         timer_rad = _util.Timer(lambda: _groundtruth.inufft_radial_approximation(F, K, Y, L, p, n))
         timer_per = _util.Timer(lambda: _nufft.inufft(F, K, Y, L, p, n, q))
 
-        times['gt'][i] = timer_gt.median
-        times['rad'][i] = timer_rad.median
-        times['per'][i] = timer_per.median
+        times['gt'][i] = _np.log10(timer_gt.median)
+        times['rad'][i] = _np.log10(timer_rad.median)
+        times['per'][i] = _np.log10(timer_per.median)
 
     _plt.figure()
     _plt.plot(Ks, times['gt'], label='Ground Truth')
     _plt.plot(Ks, times['rad'], label='Radial Approx.')
     _plt.plot(Ks, times['per'], label='Periodic Sum')
+    _plt.ylabel('Log10 Execution Time (sec.)')
+    _plt.xlabel('Bandlimit (determines problem size)')
     _plt.legend()
-    _plt.show()
+    if save:
+        _plt.savefig(_inspect.stack()[0][3] + '.pdf')
+    else:
+        _plt.show()
 
-# TODO: different test series -- with respect to what, though...? do this last

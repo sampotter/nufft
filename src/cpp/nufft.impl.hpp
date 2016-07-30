a5903f2ef9ba9df1a6b074100538b4563bb2e59c
Sam Potter <sfp@hush.ai>
2016-07-30 18:02:39 -0400

    simplify

--- src/cpp/nufft.impl.hpp
+++ src/cpp/nufft.impl.hpp
@@ -24,22 +24,25 @@ nufft::compute_P(
 	// Some preliminaries:
 
 	auto const F = values;
-	auto const Y = nodes;
+	// auto const Y = nodes; // TODO: replace this with a scaled vector version
 	auto const J = num_nodes;
-	auto const K = num_values/2; // used with least-squares collocation
-	auto const L = fmm_depth;
+	domain_t const K = num_values/2;
+	int_t const L = fmm_depth;
 	auto const N = num_values;
 	auto const p = truncation_number;
 	auto const n = neighborhood_radius;
 
 	range_t const twopi {6.283185307179586};
+	int_t const num_cells {2*n + 1};
+	auto const num_cells_recip = 1.0/static_cast<domain_t>(num_cells);
+	auto const scale_factor = num_cells_recip*(1.0/twopi);
 
 	// Initialize X_per, the periodic extension of X to the periodic
 	// summation neighborhood (X_per is computed with the correct
 	// scale immediately to prevent floating point
 	// errors---specifically, catastrophic cancellation):
 
-	int_t const X_per_size {(2*n + 1)*N};
+	int_t const X_per_size {num_cells*N};
 	std::vector<domain_t> X_per(X_per_size);
 	for (int_t i {0}; i < X_per_size; ++i) {
 		X_per[i] = static_cast<domain_t>(i)/static_cast<domain_t>(X_per_size);
@@ -62,99 +65,65 @@ nufft::compute_P(
 		Fas_sum += Fas_per[i];
 	}
 
-	// Compute N checkpoints and their periodic offsets in the
-	// interval [0, 2pi):
+	// Scale input nodes for use with FMM.
 
-	std::vector<domain_t> Yc(N);
-	std::generate(std::begin(Yc), std::end(Yc), [twopi] () {
+	std::vector<domain_t> Y(J);
+	std::transform(
+		nodes,
+		nodes + J,
+		std::begin(Y),
+		[&] (domain_t const & y) {
+			return (y + twopi*n)*scale_factor;
+		});
+#ifdef NUFFT_DEBUG
+	assert(std::is_sorted(std::cbegin(Y), std::cend(Y)));
+#endif
+
+	// Create array of checkpoints and their periodic offsets inside
+	// the scaled target domain.
+
+	std::vector<domain_t> Yc(2*N);
+	std::generate(
+		std::begin(Yc) + N,
+		std::end(Yc),
+		[n, num_cells_recip] () {
+			static auto const a = n*num_cells_recip;
+			static auto const b = (n + 1)*num_cells_recip;
 			static std::random_device dev {};
 			static std::mt19937 gen {dev()};
-			static std::uniform_real_distribution<domain_t> dist {twopi, 2*twopi};
+			static std::uniform_real_distribution<domain_t> dist {a, b};
 			return dist(gen);
 		});
-	
-	std::vector<domain_t> Yc_tilde(N);
-	for (int_t i {0}; i < N; ++i) {
-		Yc_tilde[i] = Yc[i] - twopi;
-	}
-
-	// Concatenate together Y, Yc, and Yc_tilde to prepare for use
-	// with the FMM (we also keep the initial index of our
-	// concatenated vector since we will sort it and need to be able
-	// to unsort the output of the FMM later):
-
-	std::vector<std::pair<domain_t, int_t>> Ycat_with_indices(J + 2*N);
-	int_t i {0};
-	for (; i < J; ++i) {
-		Ycat_with_indices[i] = {Y[i], i};
-	}
-	for (int_t j {0}; j < N; ++i, ++j) {
-		Ycat_with_indices[i] = {Yc[j], i};
-	}
-	for (int_t j {0}; j < N; ++i, ++j) {
-		Ycat_with_indices[i] = {Yc_tilde[j], i};
-	}
-
-	// Sort Ycat by the actual values it contains (and not the
-	// indices):
-
-	std::sort(
-		std::begin(Ycat_with_indices),
-		std::end(Ycat_with_indices),
-		[] (auto const & a, auto const & b) {
-			return a.first < b.first;
-		});
-
-	// Extract Ycat and Ycat_indices (TODO: this is awful):
-
-	std::vector<domain_t> Ycat(J + 2*N);
 	std::transform(
-		std::cbegin(Ycat_with_indices),
-		std::cend(Ycat_with_indices),
-		std::begin(Ycat),
-		[] (auto const & pair) { return pair.first; });
-
-	// Evaluate phinear using the FMM:
-
-	auto const left = -twopi*n;
-	auto const right = twopi*(n + 1);
-	auto const scale_factor = 1/(right - left);
+		std::begin(Yc) + N,
+		std::end(Yc),
+		std::begin(Yc),
+		[num_cells_recip] (domain_t const & y) {
+			return y - num_cells_recip;
+		});
 #ifdef NUFFT_DEBUG
-	assert(*std::min_element(std::cbegin(Ycat), std::cend(Ycat)) >= left);
-	assert(*std::max_element(std::cbegin(Ycat), std::cend(Ycat)) < right);
+	assert(std::is_sorted(std::cbegin(Y), std::cend(Y)));
 #endif
-	for (int_t i {0}; i < J + 2*N; ++i) {
-		Ycat[i] = scale_factor*(Ycat[i] - left);
-	}
-#ifdef NUFFT_DEBUG
-	assert(*std::min_element(std::cbegin(Ycat), std::cend(Ycat)) >= 0);
-	assert(*std::max_element(std::cbegin(Ycat), std::cend(Ycat)) < 1.0);
-	assert(*std::min_element(std::cbegin(X_per), std::cend(X_per)) >= 0);
-	assert(*std::max_element(std::cbegin(X_per), std::cend(X_per)) < 1.0);
-#endif
-	auto fmm_output = nufft::fmm1d<
+
+	// Run the FMM on the nodes and the checkpoints separately. In
+	// the future, we may be able to tune each FMM independently.
+
+	auto V = nufft::fmm1d<
 		nufft::cauchy<domain_t, std::complex<range_t>, int_t>,
 		domain_t,
 		std::complex<range_t>,
-		int_t>::fmm(X_per, Ycat, Fas_per, L, p);
-	for (int_t i {0}; i < J + 2*N; ++i) {
-		fmm_output[i] *= scale_factor;
+		int_t>::fmm(X_per, Y, Fas_per, L, p);
+	for (int_t i {0}; i < J; ++i) {
+		V[i] *= scale_factor;
 	}
 
-	// Unsort FMM output to get output associated with Y, Yc, and Yc_tilde:
-
-	std::vector<std::complex<range_t>> V(J);
-	std::vector<std::complex<range_t>> Vc(N);
-	std::vector<std::complex<range_t>> Vc_tilde(N);
-	for (int_t i {0}; i < J + 2*N; ++i) {
-		auto const j = Ycat_with_indices[i].second;
-		if (0 <= j && j < J) {
-			V[j] = fmm_output[i];
-		} else if (J <= j && j < J + N) {
-			Vc[j - J] = fmm_output[i];
-		} else if (J + N <= j && j < J + 2*N) {
-			Vc_tilde[j - J - N] = fmm_output[i];
-		}
+	auto Vc = nufft::fmm1d<
+		nufft::cauchy<domain_t, std::complex<range_t>, int_t>,
+		domain_t,
+		std::complex<range_t>,
+		int_t>::fmm(X_per, Y, Fas_per, L, p);
+	for (int_t i {0}; i < J; ++i) {
+		Vc[i] *= scale_factor;
 	}
 
 	// TODO: compute phifar.
@@ -163,9 +132,10 @@ nufft::compute_P(
 
 	// TODO: finish interpolation.
 
+	range_t const N_recip {1.0/static_cast<range_t>(N)};
 	for (int_t i {0}; i < J; ++i) {
 		auto const theta = K*nodes[i];
-		output[i] = (1.0/N)*(-Fas_sum*std::cos(theta) + 2*std::sin(theta)*V[i]);
+		output[i] = N_recip*(-Fas_sum*std::cos(theta) + 2*std::sin(theta)*V[i]);
 	}
 
 	return nufft_error::success;

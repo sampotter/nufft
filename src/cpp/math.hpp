2382d05f3202fdaff90b99f1dcdeb3d6331f5cc5
Sam Potter <sfp@hush.ai>
2016-08-04 14:36:18 -0400

    still messing around

--- src/cpp/math.hpp
+++ src/cpp/math.hpp
@@ -1,5 +1,5 @@
-#ifndef __NUFFT_MULTIPLY_HPP__
-#define __NUFFT_MULTIPLY_HPP__
+#ifndef __NUFFT_MATH_HPP__
+#define __NUFFT_MATH_HPP__
 
 #include <complex>
 #include <type_traits>
@@ -11,7 +11,7 @@ namespace nufft {
 		class S,
 		class T,
 		std::enable_if_t<is_complex<S> {} && !is_complex<T> {}> * = nullptr>
-	constexpr S multiply(S const & lhs, T const & rhs)
+	constexpr S mul(S const & lhs, T const & rhs)
 	{
 		using field_t = typename get_underlying_field<S>::type;
 		S tmp = lhs;
@@ -20,11 +20,34 @@ namespace nufft {
 		return tmp;
 	}
 
-	template <class T, std::enable_if_t<!is_complex<T> {}> * = nullptr>
-	constexpr T multiply(T const & lhs, T const & rhs)
+	template <
+		class T,
+		std::enable_if_t<!is_complex<T> {}> * = nullptr>
+	constexpr T mul(T const & lhs, T const & rhs)
 	{
 		return lhs*rhs;
 	}
+
+	template <
+		class S,
+		class T,
+		std::enable_if_t<is_complex<S> {} && is_complex<T> {}> * = nullptr>
+    constexpr void add(S & lhs, T const & rhs)
+	{
+		using S_field_t = typename get_underlying_field<S>::type;
+		using T_field_t = typename get_underlying_field<T>::type;
+		reinterpret_cast<S_field_t *>(&lhs)[0] += reinterpret_cast<T_field_t const *>(&rhs)[0];
+		reinterpret_cast<S_field_t *>(&lhs)[1] += reinterpret_cast<T_field_t const *>(&rhs)[1];
+	}
+
+	template <
+		class T,
+		std::enable_if_t<!is_complex<T> {}> * = nullptr>
+	constexpr void add(T & lhs, T const & rhs)
+	{
+		lhs += rhs;
+	}
 }
 
-#endif // __NUFFT_MULTIPLY_HPP__
+#endif // __NUFFT_MATH_HPP__
+
